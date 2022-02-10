50795e8a60c9eb611a9f3c8b5a688aeedecc3076
nova2DOT <kiiphios@hotmail.com>
2022-02-10 12:08:41 -0200

    Primeiro COmmit

---
+++ src/util/BancoDados.java
@@ -0,0 +1,26 @@
+
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class BancoDados {
+
+    public static Connection getConexao(){
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver"); //conectar com o banco de dados
+            Connection conn = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/"
+                            + "bdproduto"
+                            + "?useTimezone=true"
+                            + "&serverTimezone=America/Sao_Paulo"
+                            + "&autoReconnect=true&useSSL=false",
+                    "root", "123456");
+            return conn;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+    
+}
